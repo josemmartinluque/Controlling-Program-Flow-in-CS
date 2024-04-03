745ece9075457f108537b12cb81796c8dd22f8e6
jose.m.martin.luque <jose.m.martin.luque@avanade.com>
2024-04-03 11:48:33 +0200

    Module 3 completed

--- WiredBrainCoffeSurveys.Reports/Program.cs
+++ WiredBrainCoffeSurveys.Reports/Program.cs
@@ -6,7 +6,72 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GenerateTasksReport();
+        }
+
+        public static void GenerateTasksReport()
+        {
+            var tasks = new List<String>();
+
+            // Calculated Values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall core: {overallScore}");
+
+            // Logical Comparisons
+            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            bool customersRecommend = Q1Results.WouldRecommend >= 7;
+            bool noGranolaYesCapuccino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+            Console.WriteLine($"Coffee Score Higher Than Food: {higherCoffeeScore}");
+            Console.WriteLine($"Customers Would Recommed: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCapuccino}");
+
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipies and ingridients.");
+            }
+
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff.");
+            }
+            else
+            {
+                tasks.Add("Work with employees for improvement idead.");
+            }
+
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                tasks.Add("Reward participants with free coupon.");
+            }
+            else
+            {
+                tasks.Add("Reward participants with discount coupon.");
+            }
+
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    tasks.Add("Revisit the rewards deals.");
+                    break;
+
+                case "cleanliness":
+                    tasks.Add("Contact the cleaning vendor.");
+                    break;
+
+                case "MobileApp":
+                    tasks.Add("Contact consulting firm about it.");
+                    break;
+            }
         }
     }
 }
--- WiredBrainCoffeSurveys.Reports/obj/Debug/net8.0/WiredBrainCoffeSurveys.Reports.AssemblyInfo.cs
+++ WiredBrainCoffeSurveys.Reports/obj/Debug/net8.0/WiredBrainCoffeSurveys.Reports.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WiredBrainCoffeSurveys.Reports")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321542dfdd574b8da16af38eac854a1406736104")]
 [assembly: System.Reflection.AssemblyProductAttribute("WiredBrainCoffeSurveys.Reports")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WiredBrainCoffeSurveys.Reports")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
