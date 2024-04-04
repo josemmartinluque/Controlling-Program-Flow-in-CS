1e4f6430081ccf500ede1223a2b48386e0842a9b
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-04 08:23:54 +0200

    Added new methods

--- WiredBrainCoffeSurveys.Reports/obj/Debug/net8.0/WiredBrainCoffeSurveys.Reports.AssemblyInfo.cs
+++ WiredBrainCoffeSurveys.Reports/obj/Debug/net8.0/WiredBrainCoffeSurveys.Reports.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WiredBrainCoffeSurveys.Reports")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745ece9075457f108537b12cb81796c8dd22f8e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9eb70966088a8805051a54b57e72452e9e8099")]
 [assembly: System.Reflection.AssemblyProductAttribute("WiredBrainCoffeSurveys.Reports")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WiredBrainCoffeSurveys.Reports")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
